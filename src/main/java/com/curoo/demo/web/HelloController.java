791d890d2305bfd8f031b0f8998e846503d7f6dd
WilmarC20 <97557116+WilmarC20@users.noreply.github.com>
2025-05-29 13:49:44 -0500

    Update HelloController.java

--- src/main/java/com/curoo/demo/web/HelloController.java
+++ src/main/java/com/curoo/demo/web/HelloController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/hello")
     public String hello() {
-        return "Hola desde Spring Boot666!";
+        return "Hola desde Spring Boot777!";
     }
 }
